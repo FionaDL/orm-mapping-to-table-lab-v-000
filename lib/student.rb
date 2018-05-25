303b70d32cbd292af1db0a9d92c7675714e4f770
Fiona Lapham <fionadlapham@gmail.com>
2018-05-25 09:40:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,5 +39,12 @@ class Student
      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
+  def self.create(:name, :grade)
+    student = Students.new(name, grade)
+    student.save
+    student
+  end
+    
+
 
 end
